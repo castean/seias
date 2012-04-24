b307410adb711681bf911ce58104218ac5f174ea
Ricardo Echavarria <rgechavarria@gmail.com>
2012-04-24 11:25:05 -0600

    Nuevo Proyecto SEIAS para el DIF Estatal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seias::Application.config.secret_token = '22fa15deee631c0a1a648d357bcf89b19001c2cb3e3e836c4b76de6e79e299578245366eb60a95ade69f52681255c7a9e28eee2a7a94253d8dc74cf94c41c52a'
