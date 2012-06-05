3d15a8136fb9b09aa7c450306773d0bc3f57e093
castean <castean@hotmail.com>
2012-06-05 09:28:24 -0600

    mapas funcionando con ubicaciones

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,7 +4,11 @@ class Place < ActiveRecord::Base
   attr_accessible :address, :gmaps, :info, :latitude, :longitude, :name, :place_type, :town_id
   attr_accessor :county_id, :state_id, :country_id
   
+  acts_as_gmappable
   def gmaps4rails_address
     name 
+  end
+  def gmaps4rails_infowindow
+      "<b>#{name}</b></br>#{info}</br>#{latitude}</br>#{longitude}"# add here whatever html content you desire, it will be displayed when users clicks on the marker
   end
 end
