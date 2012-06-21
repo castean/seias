bb1e5d15606dd314596324fb187e4d8aee39e618
castean <castean@hotmail.com>
2012-06-21 13:23:47 -0600

    Poniendo Bonito programa Prioritario

--- app/models/priority_program_goal.rb
+++ app/models/priority_program_goal.rb
@@ -1,6 +1,6 @@
 class PriorityProgramGoal < ActiveRecord::Base
   attr_accessible :description, :hierarchy, :name, :priority_program_id
-  belongs_to :priority_programs
+  belongs_to :priority_program
   has_many :priority_program_strategies
   validates :name, :presence => true
   validates :description, :presence => true
