60777e5d556a1c610b2253da405ec6014a809b87
Rolando Vazquez Hernandez <rolando@rolando-Inspiron-N5110.(none)>
2012-06-25 13:20:41 -0600

    Se agrega todo lo necesario para hacer autenticacion con ldap

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -4,4 +4,8 @@ class UserSession < Authlogic::Session::Base
   def to_key
     [session_key]
   end
+
+  if Seias::Application.config.ldap_auth
+    verify_password_method :valid_ldap_credentials?
+  end
 end
