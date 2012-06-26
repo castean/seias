ddb693c1c952d623d558948898f9eb3106946abe
castean <castean@hotmail.com>
2012-06-26 09:50:20 -0600

    Cambio a español de campos, modificacion en acciones y plan estatal

--- app/models/ped_goal.rb
+++ app/models/ped_goal.rb
@@ -1,10 +1,10 @@
 class PedGoal < ActiveRecord::Base
-  attr_accessible :description, :hierarchy, :name, :ped_id, :ped_sub_theme_id
+  attr_accessible :description, :hierarchy, :name, :ped_sub_theme_id
   belongs_to :ped
   belongs_to :ped_sub_theme
   has_many :ped_strategies
   validates :name, :presence => true
   validates :description, :presence => true
   validates :hierarchy, :presence => true
-  validates :ped_id, :presence => true
+  validates :ped_sub_theme_id, :presence => true
 end
