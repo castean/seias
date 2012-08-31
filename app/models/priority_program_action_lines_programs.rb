03b990b04de11b19c8489cd4ef42d4dbcab9cac2
Cesar Reyes <creyes@difchihuahua.gob.mx>
2012-08-31 13:41:38 -0600

    merge

--- app/models/priority_program_action_lines_programs.rb
+++ app/models/priority_program_action_lines_programs.rb
@@ -1,9 +1,10 @@
 #encoding:utf-8
 class PriorityProgramActionLinesPrograms < ActiveRecord::Base
   has_and_belongs_to_many :priority_program_action_lines
+
   has_and_belongs_to_many :programs
   attr_accessible :program_id, :priority_program_action_line_id
   attr_accessor :program_id, :priority_program_action_line_id
 
-
+end
 
