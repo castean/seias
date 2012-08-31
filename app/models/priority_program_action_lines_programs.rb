f38ffef2d572bb90fd5d692235bb86902f0fe0d2
Cesar Reyes <creyes@difchihuahua.gob.mx>
2012-08-31 13:27:39 -0600

    Correccion de propriority_program_action_lines_programs.rb

--- app/models/priority_program_action_lines_programs.rb
+++ app/models/priority_program_action_lines_programs.rb
@@ -1,16 +1,7 @@
 #encoding:utf-8
 class PriorityProgramActionLinesPrograms < ActiveRecord::Base
   has_and_belongs_to_many :priority_program_action_lines
-<<<<<<< HEAD
   has_and_belongs_to_many :programs
   attr_accessible :program_id, :priority_program_action_line_id
   attr_accessor :program_id, :priority_program_action_line_id
-end
-=======
-    has_and_belongs_to_many :programs
-  attr_accessible :program_id, :priority_program_action_line_id
-  attr_accessor :program_id, :priority_program_action_line_id
-  
 
-end
->>>>>>> 3b2b43998850474e29dc909f460e7491d1206757
