45630ccb326383715c14b60d59edde5800e455a9
Cesar Reyes <creyes@difchihuahua.gob.mx>
2012-09-27 09:07:01 -0600

    Actualizando actividades en el filtro

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -10,16 +10,25 @@ module ActivitiesHelper
         f.select :activity_type_id, {}, {:prompt => '-- Seleciona el Tipo de Actividad --'}, {:id=>"tipo", :class=>"ddl_width"}
      else
 
-
+            id = 0
             pro = "Select MAX(programs.id) from activity_types INNER JOIN activity_types_critical_success_factors ON activity_types_critical_success_factors.activity_type_id = activity_types.id
             INNER JOIN critical_success_factors ON critical_success_factors.id = activity_types_critical_success_factors.critical_success_factor_id
             INNER JOIN Programs ON programs.id = critical_success_factors.program_id INNER JOIN directions ON directions.id = programs.direction_id
             WHERE  activity_types.id = #{@activity.activity_type_id}"
 
-            sql = "Select distinct activity_types.name, activity_types.id from activity_types INNER JOIN activity_types_critical_success_factors ON activity_types_critical_success_factors.activity_type_id = activity_types.id
+           id = ActiveRecord::Base.connection.select_value(pro)
+            if    id == 0 or id.nil? or id.blank?
+              sql = "Select distinct activity_types.name, activity_types.id from activity_types INNER JOIN activity_types_critical_success_factors ON activity_types_critical_success_factors.activity_type_id = activity_types.id
             INNER JOIN critical_success_factors ON critical_success_factors.id = activity_types_critical_success_factors.critical_success_factor_id
             INNER JOIN Programs ON programs.id = critical_success_factors.program_id INNER JOIN directions ON directions.id = programs.direction_id
-            WHERE programs.id = #{Program.find(ActiveRecord::Base.connection.select_value(pro)).id} order by activity_types.name"
+            WHERE programs.department_id = #{current_user.department_id} order by activity_types.name"
+            else
+              sql = "Select distinct activity_types.name, activity_types.id from activity_types INNER JOIN activity_types_critical_success_factors ON activity_types_critical_success_factors.activity_type_id = activity_types.id
+            INNER JOIN critical_success_factors ON critical_success_factors.id = activity_types_critical_success_factors.critical_success_factor_id
+            INNER JOIN Programs ON programs.id = critical_success_factors.program_id INNER JOIN directions ON directions.id = programs.direction_id
+            WHERE programs.id = #{Program.find(id).id} order by activity_types.name"
+            end
+
             #sql = "Select activity_types.name, activity_types.id from activity_types"
             filter_activity_types = ActiveRecord::Base.connection.select_rows(sql)
             filter_activity_types.map{|name, id|}
