d9ef2c04378f48aaae2c97677e32cab0dfa7ae4e
ialderete <dif.ialderete@gmail.com>
2012-11-28 13:09:10 -0700

    Revision

--- app/models/period_time_deliver.rb
+++ app/models/period_time_deliver.rb
@@ -1,6 +1,6 @@
 class PeriodTimeDeliver < ActiveRecord::Base
   belongs_to :affiliate
   belongs_to :period_time
-  has_many :benefits
+
   attr_accessible :affiliate_id, :benefit_id, :delivered, :period_number, :period_time_id
 end
